c89bc65cf54728dd6dcae5bb8907659a53c7fbbc
tinoplay <157331348+tinoplay@users.noreply.github.com>
2024-03-06 19:25:50 -0300

    atualiza

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const convertButton = document.querySelector(".convert-button")
-const valorDinheiro = document.querySelector(".select1").value
+const valorDinheiro = document.querySelector(".select1")
 
 
 function convertValores() {
@@ -7,44 +7,76 @@ function convertValores() {
     const valorReal = document.querySelector(".real-brasil")
     const valorDolar = document.querySelector(".dolar-convert")
 
-    
+
 
     const dolarDia = 5.2
     const euroDia = 6.2
-    
+    const libraDia = 6.3
 
-   
-  
-  
-   if (valorDinheiro.value == "dolar"){
-    valorDolar.innerHTML = new Intl.NumberFormat("en-US", {
+
+
+    valorReal.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
-        currency: "USD"
-    
-    }).format(inputValor/dolarDia)
+        currency: "BRL"
+    }).format(inputValor)
 
-   }
-   
-   if(valorDinheiro == "euro"){
-    valorDolar.innerHTML =  new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "EUR"
-   }).format(inputValor/euroDia)
-  
-   
-   valorReal.innerHTML = new Intl.NumberFormat("pt-br", {
-    style: "currency",
-    currency: "BRL"    
+    if (valorDinheiro.value == "Dolar") {
+        valorDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
 
-}).format(inputValor)
+        }).format(inputValor / dolarDia)
 
+    }
+
+    if (valorDinheiro.value == "Euro") {
+        valorDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValor / euroDia)
 
 
-  
-   }
+    }
+    if (valorDinheiro.value == "Libra") {
+        valorDolar.innerHTML = new Intl.NumberFormat("de-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputValor / libraDia)
+
+
+    }
 
 }
 
+    function moedaConverteda() {
+        const trocaMoedas = document.getElementById("troca-nome")
+        const trocaImg = document.querySelector(".troca-img")
+
+
+        if (valorDinheiro.value == "Dolar") {
+            trocaMoedas.innerHTML = "Dolar"
+            trocaImg.src = "./assets/dolar.png"
+        }
+
+        if (valorDinheiro.value == "Euro") {
+            trocaMoedas.innerHTML = "Euro"
+            trocaImg.src = "./assets/euro.png"
+        }
+
+        if (valorDinheiro.value == "Libra") {
+            trocaMoedas.innerHTML = "Libra"
+            trocaImg.src = "./assets/libra.png"
+        }
+
+        convertValores()
+    }
+
+    
+
+
+
+
+valorDinheiro.addEventListener("change", moedaConverteda)
 convertButton.addEventListener("click", convertValores)
 
 
